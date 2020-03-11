c01e8744d32d59eeb310670b08b651dac0832ab4
Sergiy Svitlookyi <bourneind@gmail.com>
2020-03-11 06:02:26 +0200

    Updates

---
+++ precache-manifest.2b6d67a4dbd44587054625512948ebd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be392f99230e7ce266c5ee9626f1f66",
+    "url": "/share-api-test/index.html"
+  },
+  {
+    "revision": "8db927d1b76f62c6e416",
+    "url": "/share-api-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7e6aadede9c7cbc57f88",
+    "url": "/share-api-test/static/js/2.bed310fe.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/share-api-test/static/js/2.bed310fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db927d1b76f62c6e416",
+    "url": "/share-api-test/static/js/main.c39f3d62.chunk.js"
+  },
+  {
+    "revision": "2a119f609c673c1e65f4",
+    "url": "/share-api-test/static/js/runtime-main.875fbff2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/share-api-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
