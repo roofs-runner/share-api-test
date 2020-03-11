5603ca76bde5f31e3c52eae73beabe457ee86677
Sergiy Svitlookyi <bourneind@gmail.com>
2020-03-11 05:53:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const shareData = {
+  title: 'MDN',
+  text: 'Learn web development on MDN!',
+  url: 'https://developer.mozilla.org',
+};
+
 function App() {
+
+  useEffect(() => {
+    const btn = document.querySelector('button');
+    const resultPara = document.querySelector('.result');
+    // Must be triggered some kind of "user activation"
+    btn.addEventListener('click', async () => {
+      try {
+        await navigator.share(shareData);
+        resultPara.textContent = 'MDN shared successfully'
+      } catch(err) {
+        resultPara.textContent = 'Error: ' + err
+      }
+    });
+  });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>Share file</button>
+        <div className="result">
+
+        </div>
       </header>
     </div>
   );
